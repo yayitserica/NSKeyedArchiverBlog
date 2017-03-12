cdb434c21bc05f24a1617903c13c9776e6634a4a
Erica Millado <ericamillado@gmail.com>
2017-03-11 23:28:05 -0500

    Initial Commit

---
+++ NSKeyedArchiverBlog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSKeyedArchiverBlog
+//
+//  Created by Erica Millado on 3/11/17.
+//  Copyright © 2017 Erica Millado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
