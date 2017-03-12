ec45c87cee490267c14d81741b67ee6e8fe0adbc
Erica Millado <ericamillado@gmail.com>
2017-03-12 11:22:58 -0400

    first commit

---
+++ NSKeyedArchiverBlog/DataStore.swift
@@ -0,0 +1,9 @@
+//
+//  DataStore.swift
+//  NSKeyedArchiverBlog
+//
+//  Created by Erica Millado on 3/12/17.
+//  Copyright © 2017 Erica Millado. All rights reserved.
+//
+
+import Foundation
---
+++ NSKeyedArchiverBlog/ShoppingItem.swift
@@ -0,0 +1,9 @@
+//
+//  ShoppingItem.swift
+//  NSKeyedArchiverBlog
+//
+//  Created by Erica Millado on 3/11/17.
+//  Copyright © 2017 Erica Millado. All rights reserved.
+//
+
+import Foundation
